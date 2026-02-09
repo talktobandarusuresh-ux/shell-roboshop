af578920e739939d91d06e1f762c10830edb28dc
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-02-09 15:49:01 +0530

    shell

--- payment.sh
+++ payment.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.bandarusuresh.in.fun
+MONGODB_HOST=mongodb.bandarusuresh.in
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.bandarusuresh.in.fun
+MYSQL_HOST=mysql.bandarusuresh.in
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
